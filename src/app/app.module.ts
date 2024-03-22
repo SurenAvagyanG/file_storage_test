50cb2839f2542662781add09d546117763ada72d
suren <suren@fifth-llc.com>
2024-03-22 16:41:13 +0400

    feat: setup database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MainModule } from '@feature/main/main.module';
 import { ErrorHandlerModule } from '@core/error-handler/error-handler.module';
 import { appConfig } from '@config/app.config';
 import { dbConfig } from '@config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig, dbConfig],
+      isGlobal: true,
     }),
     ErrorHandlerModule,
     MainModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) =>
+        config.get(`db.${config.get('db.driver')}`),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class AppModule {}
