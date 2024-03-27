fabee7d358180220b714854a7b37603d4986133c
suren <suren@fifth-llc.com>
2024-03-27 19:40:25 +0400

    fix: tests

--- src/app/feature/attachment/attachment.resolver.spec.ts
+++ src/app/feature/attachment/attachment.resolver.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AttachmentResolver } from './attachment.resolver';
-import { AttachmentService } from './attachment.service';
+import { AttachmentModule } from '@feature/attachment/attachment.module';
+import { DatabaseTestingModule } from '@shared/database';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from '@config/app.config';
+import { dbConfig } from '@config/db.config';
+import { fileSystemConfig } from '@config/file-system.config';
+import { StorageFactory } from '@infrastructure/storage';
+import { MockFileSystemAdapter } from '@infrastructure/storage/mock-file-system/mock-file-system-adapter';
 
 describe('AttachmentResolver', () => {
   let resolver: AttachmentResolver;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AttachmentResolver, AttachmentService],
+      imports: [
+        AttachmentModule,
+        DatabaseTestingModule,
+        ConfigModule.forRoot({
+          load: [appConfig, dbConfig, fileSystemConfig],
+          isGlobal: true,
+        }),
+      ],
     }).compile();
 
+    StorageFactory.addStorageDriver('s3', new MockFileSystemAdapter());
     resolver = module.get<AttachmentResolver>(AttachmentResolver);
   });
 
--- src/app/shared/database/type-orm/database.test.module.ts
+++ src/app/shared/database/type-orm/database.test.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { appConfig } from '@config/app.config';
-import { dbConfig } from '@config/db.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [appConfig, dbConfig],
-      isGlobal: true,
-    }),
+    ConfigModule,
     TypeOrmModule.forRootAsync({
       useFactory: (config: ConfigService) =>
         config.getOrThrow(`db.${config.getOrThrow('db.driver')}`),
