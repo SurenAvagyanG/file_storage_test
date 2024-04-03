027001c6aa78dc61e69ba697bf8b97e72decf7cd
SurenAvagyanXP <suren.avagyan@experianto.com>
2024-04-04 02:14:55 +0400

    fix: add postgre for GH actions

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,14 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('../../package.json');
-
-// Get the version from package.json
-const version = packageJson.version;
 export const appConfig = registerAs('app', () => ({
   port: process.env.APP_PORT ? Number(process.env.APP_PORT) : 3030,
   host: process.env.APP_HOST ?? '0.0.0.0',
-  version: version,
+  version: '0.0.0.0',
   environment: process.env.APP_ENVIRONMENT ?? 'LOCAL',
   global_prefix: 'file-storage',
 }));
