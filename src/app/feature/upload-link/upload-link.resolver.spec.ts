f9dd80c79faa2d860212f6d5d24711380ea89443
suren-norekyan <suren.norekyan@act-on.com>
2024-06-24 19:59:25 +0400

    #EX-106: Implement missing upload link tests for File Storage service

--- src/app/feature/upload-link/upload-link.resolver.spec.ts
+++ src/app/feature/upload-link/upload-link.resolver.spec.ts
@@ -8,9 +8,12 @@ import { MockFileSystemAdapter } from '@infrastructure/storage/mock-file-system/
 import { UploadLinkResolver } from '@feature/upload-link/upload-link.resolver';
 import { DatabaseTestingModule } from '@infrastructure/common';
 import { UploadLinkModule } from '@feature/upload-link/upload-link.module';
+import { StorageService } from '@shared/storage';
 
 describe('AttachmentResolver', () => {
   let resolver: UploadLinkResolver;
+  let storageService: StorageService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
@@ -24,6 +27,7 @@ describe('AttachmentResolver', () => {
     }).compile();
 
     StorageFactory.addStorageDriver('s3', new MockFileSystemAdapter());
+    storageService = module.get<StorageService>(StorageService);
     resolver = module.get<UploadLinkResolver>(UploadLinkResolver);
   });
 
@@ -31,15 +35,42 @@ describe('AttachmentResolver', () => {
     expect(resolver).toBeDefined();
   });
 
-  // it('should create upload link with extension', async () => {
-  //   const extension = 'png';
-  //
-  //   const entity = await resolver.createUploadLink({
-  //     extension,
-  //   });
-  //
-  //   expect(entity.signedUrl).toContain(extension);
-  //   expect(entity.staticUrl).toBeDefined();
-  //   expect(entity.id).toBeDefined();
-  // });
+  it('should create an upload link with valid extension and parameters', async () => {
+    const createUploadLinkInput = {
+      extension: 'jpg',
+      params: { expires: 3600, contentType: 'image/jpeg' },
+    };
+
+    const result = await resolver.createUploadLink(createUploadLinkInput);
+    expect(result.signedUrl).toContain(
+      `${createUploadLinkInput.extension}?signed=1`,
+    );
+    expect(result.staticUrl).toContain(createUploadLinkInput.extension);
+    expect(result.staticUrl).not.toContain('signed');
+    expect(result.id).toBeDefined();
+  });
+
+  it('should handle error when invalid extension is provided', async () => {
+    const createUploadLinkInput = { extension: 'invalid_extension' };
+
+    await expect(
+      resolver.createUploadLink(createUploadLinkInput),
+    ).rejects.toThrow('Invalid extension');
+  });
+
+  it('should handle error when storage service fails to generate upload URL', async () => {
+    jest
+      .spyOn(storageService, 'getUploadUrl')
+      .mockRejectedValueOnce(new Error('Storage service error'));
+
+    const createUploadLinkInput = { extension: 'png' };
+
+    await expect(
+      resolver.createUploadLink(createUploadLinkInput),
+    ).rejects.toThrow('Storage service error');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 });
--- src/infrastructure/storage/mock-file-system/mock-file-system-adapter.ts
+++ src/infrastructure/storage/mock-file-system/mock-file-system-adapter.ts
@@ -5,6 +5,7 @@ import {
   UploadUrlResponse,
   UrlParams,
 } from '../storage.interface';
+import { ImageExtensions, VideoExtensions } from '@domain/constants';
 
 export class MockFileSystemAdapter implements IStorage {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -13,9 +14,17 @@ export class MockFileSystemAdapter implements IStorage {
   }
 
   getUploadUrl(extension: string): Promise<UploadUrlResponse> {
-    return Promise.resolve({
-      signedUrl: `http//string.${extension}?signed=1`,
-      staticUrl: `http//string.${extension}`,
+    const validExtensions = [...ImageExtensions, ...VideoExtensions];
+
+    return new Promise((resolve, reject) => {
+      if (!validExtensions.includes(extension)) {
+        reject(new Error('Invalid extension'));
+      } else {
+        resolve({
+          signedUrl: `http//string.${extension}?signed=1`,
+          staticUrl: `http//string.${extension}`,
+        });
+      }
     });
   }
 
